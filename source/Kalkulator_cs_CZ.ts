cd4efe660290681e177fc4dcec453a22d4f88c50
Martin Novák <martin.novak@gmx.com>
2022-02-26 15:47:00 +0100

    Úprava překladu do čj

--- source/Kalkulator_cs_CZ.ts
+++ source/Kalkulator_cs_CZ.ts
@@ -4,147 +4,168 @@
 <context>
     <name>Kalkulator</name>
     <message>
-        <location filename="kalkulator.cpp" line="21"/>
-        <location filename="kalkulator.cpp" line="34"/>
-        <location filename="kalkulator.cpp" line="270"/>
-        <location filename="kalkulator.cpp" line="271"/>
+        <location filename="kalkulator.cpp" line="20"/>
+        <location filename="kalkulator.cpp" line="33"/>
+        <location filename="kalkulator.cpp" line="299"/>
+        <location filename="kalkulator.cpp" line="300"/>
         <source>Calculator</source>
         <translation>Kalkulačka</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="24"/>
-        <location filename="kalkulator.cpp" line="326"/>
+        <location filename="kalkulator.cpp" line="23"/>
+        <location filename="kalkulator.cpp" line="354"/>
         <source>Calculator with up to 200 integer digits and 100 decimal digits</source>
         <translation>Kalkulačka s až 200 celočíselnými a 100 desetinnými číslicemi</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="48"/>
+        <location filename="kalkulator.cpp" line="43"/>
         <source>The constant π (pi).</source>
         <translation>Ludolfova konstanta pí.</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="50"/>
+        <location filename="kalkulator.cpp" line="45"/>
         <source>Sine</source>
         <translation>Sinus</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="52"/>
+        <location filename="kalkulator.cpp" line="47"/>
         <source>Cosine</source>
         <translation>Cosinus</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="54"/>
+        <location filename="kalkulator.cpp" line="49"/>
         <source>(</source>
         <translation>levá závorka - (</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="56"/>
+        <location filename="kalkulator.cpp" line="51"/>
         <source>)</source>
         <translation>pravá závorka - )</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="67"/>
+        <location filename="kalkulator.cpp" line="62"/>
         <source>The Euler&apos;s constant e.</source>
         <translation>Eulerova konstanta e.</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="69"/>
+        <location filename="kalkulator.cpp" line="64"/>
         <source>Natural logarithm</source>
         <translation>Přirozený logaritmus</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="71"/>
+        <location filename="kalkulator.cpp" line="66"/>
         <source>Logarithm</source>
         <translation>Logaritmus</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="73"/>
+        <location filename="kalkulator.cpp" line="68"/>
         <source>Exponentiation</source>
         <translation>Mocnina</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="75"/>
+        <location filename="kalkulator.cpp" line="70"/>
         <source>Division</source>
         <translation>Děleno</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="85"/>
+        <location filename="kalkulator.cpp" line="80"/>
         <source>All clear</source>
         <translation>Vše vyčistit</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="90"/>
+        <location filename="kalkulator.cpp" line="85"/>
         <source>multiplication</source>
         <translation>Krát</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="101"/>
+        <location filename="kalkulator.cpp" line="96"/>
         <source>BackSpace</source>
         <translation>BackSpace</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="106"/>
+        <location filename="kalkulator.cpp" line="101"/>
         <source>Minus</source>
         <translation>Mínus</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="116"/>
+        <location filename="kalkulator.cpp" line="111"/>
         <source>Modulo</source>
         <translation>Zbytek po dělení</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="121"/>
+        <location filename="kalkulator.cpp" line="116"/>
         <source>Plus</source>
         <translation>Plus</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="131"/>
+        <location filename="kalkulator.cpp" line="126"/>
         <source>Factorial</source>
         <translation>Faktoriál</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="133"/>
+        <location filename="kalkulator.cpp" line="128"/>
         <source>Square root</source>
         <translation>Druhá odmocnina</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="135"/>
-        <location filename="kalkulator.cpp" line="266"/>
+        <location filename="kalkulator.cpp" line="130"/>
+        <location filename="kalkulator.cpp" line="295"/>
         <source>.</source>
         <translation>,</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="136"/>
+        <location filename="kalkulator.cpp" line="131"/>
         <source>Decimal point</source>
         <translation>Desetinná čárka</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="138"/>
+        <location filename="kalkulator.cpp" line="133"/>
         <source>Calculate</source>
         <translation>Vypočítat</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="148"/>
+        <location filename="kalkulator.cpp" line="143"/>
         <source>decimal</source>
         <translation>desetinný</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="150"/>
+        <location filename="kalkulator.cpp" line="145"/>
         <source>integral</source>
         <translation>celočíselný</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="151"/>
+        <location filename="kalkulator.cpp" line="146"/>
         <source>Number of decimal digits</source>
         <translation>Počet desetinných číslic</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="155"/>
+        <location filename="kalkulator.cpp" line="150"/>
         <source>Set settings</source>
         <translation>Uložit nastavení</translation>
     </message>
     <message>
-        <location filename="kalkulator.cpp" line="328"/>
+        <location filename="kalkulator.cpp" line="159"/>
+        <location filename="kalkulator.cpp" line="239"/>
+        <source>Hide history</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="kalkulator.cpp" line="160"/>
+        <source>Remove item</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="kalkulator.cpp" line="161"/>
+        <source>Clear history</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="kalkulator.cpp" line="234"/>
+        <source>Show history</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="kalkulator.cpp" line="356"/>
         <source>Error</source>
         <translation>Error</translation>
     </message>
@@ -214,15 +235,16 @@
         <location filename="Number.cpp" line="315"/>
         <location filename="Number.cpp" line="323"/>
         <location filename="Number.cpp" line="354"/>
-        <location filename="Number.cpp" line="720"/>
-        <location filename="Number.cpp" line="755"/>
-        <location filename="Number.cpp" line="903"/>
+        <location filename="Number.cpp" line="723"/>
+        <location filename="Number.cpp" line="758"/>
+        <location filename="Number.cpp" line="906"/>
         <source>An overflow occurs!</source>
         <translation>Došlo k přetečení.</translation>
     </message>
     <message>
         <location filename="Number.cpp" line="402"/>
         <location filename="Number.cpp" line="495"/>
+        <location filename="Number.cpp" line="578"/>
         <source>Dividing by zero!</source>
         <translation>Dělení nulou.</translation>
     </message>
